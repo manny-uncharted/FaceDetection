e3847b0525d0e193928d2b36f324b7d4d1349d15
manny-uncharted <omoebun52@gmail.com>
2022-09-25 17:30:43 +0100

    done

--- faceDetectionModule.py
+++ faceDetectionModule.py
@@ -23,8 +23,9 @@ class FaceDetector():
                 ih, iw, ic = img.shape
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
                 bboxs.append([id, bbox, detection.score])
-                self.fancyDraw(img, bbox)
-                cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+                if draw:
+                    self.fancyDraw(img, bbox)
+                    cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
         return img, bboxs
     
@@ -42,8 +43,22 @@ class FaceDetector():
         x1, y1 = x + w, y + h
 
         cv2.rectangle(img, bbox, (255, 0, 255), rt)
+
+        # Top Left  x,y
         cv2.line(img, (x,y), (x+l, y), (255, 0, 255), t)
         cv2.line(img, (x,y), (x, y+l), (255, 0, 255), t)
+
+        # Top Right  x1,y
+        cv2.line(img, (x1,y), (x1-l, y), (255, 0, 255), t)
+        cv2.line(img, (x1,y), (x1, y+l), (255, 0, 255), t)
+
+        # Bottom Left  x,y1
+        cv2.line(img, (x,y1), (x+l, y1), (255, 0, 255), t)
+        cv2.line(img, (x,y1), (x, y1-l), (255, 0, 255), t)
+
+        # Bottom Right  x1,y1
+        cv2.line(img, (x1,y1), (x1-l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1,y1), (x1, y1-l), (255, 0, 255), t)
 
         return img
 
